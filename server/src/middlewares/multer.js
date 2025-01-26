31d68e46c92d8e7f99c8a415b7f911015ebac3d5
Aniket Gupta <aniketgupta.org@gmail.com>
2025-01-27 03:14:04 +0530

    upload fixh

--- server/src/middlewares/multer.js
+++ server/src/middlewares/multer.js
@@ -1,26 +1,6 @@
-// import multer from 'multer';
-
-// export const uploader = multer({
-//   // limits: { fileSize: 10000000 },  // 2 MB
-//   fileFilter: (req, file, cb) => {
-//     if (file.mimetype.startsWith('image/')) {
-//       cb(null, true);
-//     } else {
-//       cb(new Error('Please upload only images.'), false);
-//     }
-//   },
-//   storage: multer.diskStorage({
-//     destination: (req, file, cb) => {
-//       cb(null, 'uploads/'); 
-//     },
-//     filename: (req, file, cb) => {
-//       cb(null, file.originalname); 
-//     }
-//   })
-// });
-
-
 import multer from 'multer';
-import path from 'path';
 
-export const uploader = multer({ dest: 'uploads/' });  
+export const uploader = multer({
+  storage: multer.memoryStorage(), // Store file in memory as a Buffer
+  limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5 MB
+});
